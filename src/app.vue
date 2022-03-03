6b589f0114ee0411bea0832ccb93658429238695
aziwang <aziwang@tencent.com>
2022-03-03 16:36:10 +0800

    feat: 样式

--- src/app.vue
+++ src/app.vue
@@ -2,51 +2,62 @@
   <div>
     <el-container>
       <el-main>
-        <div>
-          <el-button class="drink-btn" type="primary" size="default" @click="handleDrink">
-            Click to drink 🍺 🍷 🍸 🍹 🍾 🍶 🍼 🥂 🥃 🧋 </el-button
+        <div class="container">
+          <el-button
+            class="drink-btn"
+            type="primary"
+            size="default"
+            @click="handleDrink"
           >
+            Click to drink 🍺 🍷 🍸 🍹 🍾 🍶 🍼 🥂 🥃 🧋
+          </el-button>
+          <el-tag v-if="alcohol" class="ml-2" type="success">{{
+            alcohol
+          }}</el-tag>
         </div>
-        <el-tag v-if="alcohol" class="ml-2" type="success">{{ alcohol }}</el-tag>
       </el-main>
     </el-container>
   </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data() {
     return {
-      alcohol: '',
+      alcohol: "",
       isLoading: false,
     };
   },
   methods: {
     handleDrink() {
       axios({
-        method: 'get',
-        url: '/drink',
+        method: "get",
+        url: "/drink",
       }).then((res) => {
         console.log(res);
         this.alcohol = res.data;
         this.$message({
           message: `今天喝${this.alcohol}`,
-          type: 'success',
-        })
-      })
+          type: "success",
+        });
+      });
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.drink-btn {
+.container {
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+
+.drink-btn {
+  margin-bottom: 10px;
 }
 .greeting {
   color: lightblue;
