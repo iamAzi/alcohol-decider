ccbdabff5cb0df3a7f14ad44aeded5bb71b1eac8
aziwang <aziwang@tencent.com>
2022-03-15 13:19:27 +0800

    feat: axios get

--- src/app.vue
+++ src/app.vue
@@ -43,7 +43,7 @@ export default {
       axios({
         method: 'get',
         url: '/drink',
-        data: {
+        params: {
           curDrink: this.alcohol.name,
         },
       }).then((res) => {
